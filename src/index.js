d2597446fd1ff77c7c192c1719e98a5e19c14bd0
zh-lx <18366276315@163.com>
2021-11-04 07:55:50 +0800

    (一): 初始化项目

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class ClassComp extends Component {
+  render() {
+    return <div>this is a class-component</div>;
+  }
+}
+
+function FunctionComp() {
+  return <div>this is a function-component</div>;
+}
+
+const jsx = (
+  <div>
+    root div
+    <ClassComp />
+    <FunctionComp />
+    <div className="box">
+      this is div level1
+      <a href="https://github.com/zh-lx/mini-react"> mini react link</a>
+      <p style={{ color: 'red' }}>this is a p</p>
+      <>
+        this is fragment
+        <div>this is div level2.1</div>
+        <div>this is div level2.2</div>
+      </>
+    </div>
+    <div>
+      this is condition render
+      {true && <div>condition true</div>}
+      {false && <div>condition false</div>}
+      <input
+        type="button"
+        value="Hello"
+        onClick={() => {
+          alert('hello');
+        }}
+      />
+    </div>
+    {['item1', 'item2', 'item3'].map((item) => (
+      <li key={item}>{item}</li>
+    ))}
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(jsx, document.getElementById('root'));
