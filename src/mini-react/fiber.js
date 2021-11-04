3025da5afcc56ab13e7c631961e4c15a586ede32
zh-lx <18366276315@163.com>
2021-11-04 08:47:38 +0800

    (四): 3.递归改成迭代

--- src/mini-react/fiber.js
+++ src/mini-react/fiber.js
@@ -26,4 +26,78 @@ function performUnitOfWork(workInProgress) {
     // 如果 fiber 有父 fiber，将当前 dom 挂载到父 fiber 的 stateNode 下
     workInProgress.return.stateNode.appendChild(workInProgress.stateNode);
   }
+
+  // 迭代处理函数组件、类组件、列表渲染和 children 等情况
+  const children = workInProgress.element?.props?.children;
+  if (children) {
+    let elements = Array.isArray(children) ? children : [children];
+    elements = handleElements(elements);
+    // element 存在，则遍历其挂载到 fiber 树上
+    let index = 0; // 当前遍历的子元素在父节点下的下标
+    let prevSibling = null; // 记录上一个兄弟节点
+
+    while (index < elements.length) {
+      // 遍历子元素
+      const element = elements[index];
+      // 创建新的 fiber
+      const newFiber = {
+        element: Array.isArray(element) // 针对嵌套数组处理
+          ? {
+              type: Symbol('react.fragment'),
+              props: {
+                children: element,
+              },
+            }
+          : element,
+        return: workInProgress,
+        stateNode: null,
+      };
+      if (index === 0) {
+        // 如果下标为 0，则将当前fiber设置为父 fiber 的 child
+        workInProgress.child = newFiber;
+      } else {
+        // 否则通过 sibling 作为兄弟 fiber 连接
+        prevSibling.sibling = newFiber;
+      }
+      prevSibling = newFiber;
+      index++;
+    }
+  }
+}
+
+// 列表渲染、条件渲染、React 组件等情况进行处理
+function handleElements(children) {
+  let result = [];
+  children.forEach((child) => {
+    if (Array.isArray(child)) {
+      // 如果 child 时数组，打平数组
+      result = result.concat(handleElements(child));
+    } else if (child?.type?.toString() === 'Symbol(react.fragment)') {
+      // fragment 处理，直接对其 children 处理
+      let children = child.props.children;
+      if (children) {
+        const elements = Array.isArray(children) ? children : [children];
+        result = result.concat(handleElements(elements));
+      }
+    } else if (typeof child?.type === 'function') {
+      // react 组件的处理
+      if (child?.type.prototype.isReactComponent) {
+        // 类组件
+        const { props, type: Comp } = child;
+        const component = new Comp(props);
+        const jsx = component.render();
+        result = result.concat(handleElements([jsx]));
+      } else {
+        // 函数组件
+        const { props, type: Fn } = child;
+        const jsx = Fn(props);
+        result = result.concat(handleElements([jsx]));
+      }
+    } else if (!child) {
+      // 条件渲染为假时，忽略其渲染
+    } else {
+      result.push(child);
+    }
+  });
+  return result;
 }
--- src/mini-react/react-dom.js
+++ src/mini-react/react-dom.js
@@ -8,23 +8,9 @@ function render(element, container) {
 export function renderDom(element) {
   let dom = null; // 要返回的 dom
 
-  if (!element) {
-    return document.createDocumentFragment();
-  }
-
   if (typeof element === 'string') {
     // 如果 element 本身为 string，表示文本节点，不需要考虑其 props 直接返回
     dom = document.createTextNode(element);
-    return dom;
-  }
-
-  if (Array.isArray(element)) {
-    // 数组类型的渲染
-    dom = document.createDocumentFragment();
-    for (let item of element) {
-      const child = renderDom(item);
-      dom.appendChild(child);
-    }
     return dom;
   }
 
@@ -36,31 +22,8 @@ export function renderDom(element) {
   if (typeof type === 'string') {
     // 常规 dom 节点的渲染
     dom = document.createElement(type);
-  } else if (typeof type === 'function') {
-    // React组件的渲染
-    if (type.prototype.isReactComponent) {
-      // 类组件
-      const { props, type: Comp } = element;
-      const component = new Comp(props);
-      const jsx = component.render();
-      dom = renderDom(jsx);
-    } else {
-      // 函数组件
-      const { props, type: Fn } = element;
-      const jsx = Fn(props);
-      dom = renderDom(jsx);
-    }
-  } else if (type.toString() === 'Symbol(react.fragment)') {
-    // React.Fragment 的渲染
-    dom = document.createDocumentFragment();
   } else {
     // 其他情况暂不考虑
-  }
-
-  if (children) {
-    // children 存在，对子节点递归渲染
-    const childrenDom = renderDom(children);
-    dom.appendChild(childrenDom);
   }
 
   updateAttributes(dom, attributes);
