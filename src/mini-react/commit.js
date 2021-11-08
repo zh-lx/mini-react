5809e257d74c6eba20414011ba4ab04791c2b32e
zh-lx <18366276315@163.com>
2021-11-08 22:39:56 +0800

    五: 6.Deletion — 移除 dom

--- src/mini-react/commit.js
+++ src/mini-react/commit.js
@@ -1,7 +1,11 @@
 import { updateAttributes } from './react-dom';
+import { getDeletions } from './fiber';
 
 // 从根节点开始 commit
 export function commitRoot(rootFiber) {
+  const deletions = getDeletions();
+  deletions.forEach(commitWork);
+
   commitWork(rootFiber.child);
 }
 
@@ -10,9 +14,17 @@ function commitWork(fiber) {
   if (!fiber) {
     return;
   }
+
+  let parentDom = fiber.return.stateNode;
+  if (fiber.flag === 'Deletion') {
+    if (typeof fiber.element?.type !== 'function') {
+      parentDom.removeChild(fiber.stateNode);
+    }
+    return;
+  }
+
   // 深度优先遍历，先遍历 child，后遍历 sibling
   commitWork(fiber.child);
-  let parentDom = fiber.return.stateNode;
   if (fiber.flag === 'Placement') {
     // 添加 dom
     const targetPositionDom = parentDom.childNodes[fiber.index]; // 要插入到那个 dom 之前
