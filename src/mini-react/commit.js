aa9d03aca3cf5dbcc3f9152fbf829240327854c9
zh-lx <18366276315@163.com>
2021-11-08 21:48:01 +0800

    五: 4.Placement — 添加 dom

--- src/mini-react/commit.js
+++ src/mini-react/commit.js
@@ -11,8 +11,17 @@ function commitWork(fiber) {
   // 深度优先遍历，先遍历 child，后遍历 sibling
   commitWork(fiber.child);
   let parentDom = fiber.return.stateNode;
-  if (fiber.stateNode) {
-    parentDom.appendChild(fiber.stateNode);
+  if (fiber.flag === 'Placement') {
+    // 添加 dom
+    const targetPositionDom = parentDom.childNodes[fiber.index]; // 要插入到那个 dom 之前
+    if (targetPositionDom) {
+      // targetPositionDom 存在，则插入
+      parentDom.insertBefore(fiber.stateNode, targetPositionDom);
+    } else {
+      // targetPositionDom 不存在，插入到最后
+      parentDom.appendChild(fiber.stateNode);
+    }
   }
+
   commitWork(fiber.sibling);
 }
