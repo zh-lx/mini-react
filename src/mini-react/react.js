b796bca22792a598b9ac499ecdf9748d2f25aadf
zh-lx <18366276315@163.com>
2021-11-09 21:50:45 +0800

    七: 实现 hooks

--- src/mini-react/react.js
+++ src/mini-react/react.js
@@ -1,4 +1,4 @@
-import { commitRender } from './fiber';
+import { commitRender, getCurrentFunctionFiber, getHookIndex } from './fiber';
 export class Component {
   constructor(props) {
     this.props = props;
@@ -26,3 +26,34 @@ Component.prototype.setState = function (param) {
 Component.prototype._UpdateProps = function (props) {
   this.props = props;
 };
+
+export function useState(initial) {
+  const currentFunctionFiber = getCurrentFunctionFiber();
+  const hookIndex = getHookIndex();
+  // 取当前执行的函数组件之前的 hook
+  const oldHook = currentFunctionFiber?.alternate?.hooks?.[hookIndex];
+
+  // oldHook存在，取之前的值，否则取现在的值
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [], // 一次函数执行过程中可能调用多次 setState，将其放进队列一并执行
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach((action) => {
+    hook.state = action(hook.state);
+  });
+
+  const setState = (action) => {
+    if (typeof action === 'function') {
+      hook.queue.push(action);
+    } else {
+      hook.queue.push(() => {
+        return action;
+      });
+    }
+    commitRender();
+  };
+  currentFunctionFiber.hooks.push(hook);
+  return [hook.state, setState];
+}
