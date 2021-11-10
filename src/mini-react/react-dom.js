db1ae9a9e5c0b5816803c500aabba5e8d6744fc0
zh-lx <18366276315@163.com>
2021-11-10 20:48:33 +0800

    三: 2.递归改成迭代

--- src/mini-react/react-dom.js
+++ src/mini-react/react-dom.js
@@ -5,7 +5,7 @@ function render(element, container) {
 }
 
 // 将 React.Element 渲染为真实 dom
-function renderDom(element) {
+export function renderDom(element) {
   let dom = null; // 要返回的 dom
 
   if (!element && element !== 0) {
@@ -25,18 +25,6 @@ function renderDom(element) {
     return dom;
   }
 
-  if (Array.isArray(element)) {
-    // 列表渲染
-    dom = document.createDocumentFragment();
-    for (let item of element) {
-      const child = renderDom(item);
-      if (child) {
-        dom.appendChild(child);
-      }
-    }
-    return dom;
-  }
-
   const {
     type,
     props: { children, ...attributes },
@@ -45,31 +33,9 @@ function renderDom(element) {
   if (typeof type === 'string') {
     // 常规 dom 节点的渲染
     dom = document.createElement(type);
-  } else if (typeof type === 'function') {
-    // React组件的渲染
-    if (type.prototype.isReactComponent) {
-      // 类组件
-      const { props, type: Comp } = element;
-      const component = new Comp(props);
-      const jsx = component.render();
-      dom = renderDom(jsx);
-    } else {
-      // 函数组件
-      const { props, type: Fn } = element;
-      const jsx = Fn(props);
-      dom = renderDom(jsx);
-    }
   } else {
     // 其他情况暂不考虑
     return null;
-  }
-
-  if (children) {
-    // children 存在，对子节点递归渲染
-    const childrenDom = renderDom(children);
-    if (childrenDom) {
-      dom.appendChild(childrenDom);
-    }
   }
 
   updateAttributes(dom, attributes);
